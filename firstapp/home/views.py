30377bb0ee8bbe61928dd1f1c55204dc068a0e06
Ali <ali.moayer5879@gmail.com>
2023-06-19 20:03:27 +0300

    Creating files and directories to get user browser information

--- firstapp/home/views.py
+++ firstapp/home/views.py
@@ -7,3 +7,8 @@ from django.http import HttpResponse
 def say_hello(request):
     return HttpResponse('Hello World Django')
 
+
+def browser_info(request):
+    user_agent = request.META.get('HTTP_USER_AGENT', '')
+    return render(request, 'browser_info.html', {'browser_name': user_agent})
+
