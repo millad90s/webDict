4f57a24236855f1fd2c9336c9a723775500c665e
Ali <ali.moayer5879@gmail.com>
2023-06-25 17:43:35 +0300

    Update URLs and views, modify browser_info.html template for show the user's browser information, IP and country

--- firstapp/home/views.py
+++ firstapp/home/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+import requests
+import ipstack
+from ipstack import GeoLookup
+from ip3country import CountryLookup
 
 
 # Create your views here.
@@ -12,3 +16,20 @@ def browser_info(request):
     user_agent = request.META.get('HTTP_USER_AGENT', '')
     return render(request, 'browser_info.html', {'browser_name': user_agent})
 
+
+def ip_user(request):
+    user_info = request.META.get('REMOTE_ADDR')
+    return render(request, 'browser_info.html', {'browser_ip': user_info})
+
+
+def show_info(request):
+    user_agent = request.META.get('HTTP_USER_AGENT', '')
+    user_ip = request.META.get('REMOTE_ADDR')
+    # user_ip = '159.146.73.217'
+    lookup = CountryLookup()
+    country_code = lookup.lookupStr(user_ip)
+    data = {
+        'browser_name': user_agent, 'user_ip': user_ip, 'country_code': country_code,
+    }
+
+    return render(request, 'browser_info.html', data)
