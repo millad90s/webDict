28774a51ff443016b6cc2afa024d96f436083161
Ali <ali.moayer5879@gmail.com>
2023-07-02 02:35:39 +0300

    Create app Poll

---
+++ firstapp/polls/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+class Question(models.Model):
+    question = models.TextField()
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField()
+
+
+class Option(models.Model):
+    id_question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    option = models.TextField()
---
+++ firstapp/polls/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question
+
+
+def say_hello(request):
+    return HttpResponse('Hello poll')
+
+
+def template_poll(request):
+    return render(request, 'poll_info.html')
+
+
+def list_poll(request):
+    question = Question.objects.all()
+    # print(question)
+    return render(request, 'poll_info.html', {'all_questions': question})
+
+
